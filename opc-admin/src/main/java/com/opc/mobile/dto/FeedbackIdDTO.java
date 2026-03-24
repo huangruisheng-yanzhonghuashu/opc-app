cf64346d058f4199b9fafada5d4df6993d358b41
huangruisheng-yanzhonghuashu <ruishengle@qq.com>
2026-03-24 13:41:22 +0800

    搜索热词

--- opc-admin/src/main/java/com/opc/mobile/dto/FeedbackIdDTO.java
+++ opc-admin/src/main/java/com/opc/mobile/dto/FeedbackIdDTO.java
@@ -1,12 +1,15 @@
 package com.opc.mobile.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 /**
  * 反馈ID请求DTO
  */
+@Schema(description = "反馈ID请求")
 public class FeedbackIdDTO
 {
+    @Schema(description = "反馈ID", required = true, example = "1")
     @NotNull(message = "反馈ID不能为空")
     private Long id;
 
--- opc-admin/src/main/java/com/opc/mobile/dto/OrderIdDTO.java
+++ opc-admin/src/main/java/com/opc/mobile/dto/OrderIdDTO.java
@@ -1,12 +1,15 @@
 package com.opc.mobile.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 /**
  * 订单ID请求DTO
  */
+@Schema(description = "订单ID请求")
 public class OrderIdDTO
 {
+    @Schema(description = "订单ID", required = true, example = "1")
     @NotNull(message = "订单ID不能为空")
     private Long id;
 
