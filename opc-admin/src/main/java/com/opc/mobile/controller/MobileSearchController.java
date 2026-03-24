626f063c2c9efb818e3f39424377a831cd4ed29a
huangruisheng-yanzhonghuashu <ruishengle@qq.com>
2026-03-24 14:14:02 +0800

    全局搜索

--- opc-admin/src/main/java/com/opc/mobile/controller/MobileSearchController.java
+++ opc-admin/src/main/java/com/opc/mobile/controller/MobileSearchController.java
@@ -9,10 +9,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.opc.common.core.controller.BaseController;
 import com.opc.common.core.page.TableDataInfo;
+import com.opc.core.domain.CoreMaterial;
 import com.opc.core.domain.CoreSearchHotword;
-import com.opc.mobile.dto.SearchHotwordQueryDTO;
+import com.opc.core.service.ICoreMaterialService;
 import com.opc.core.service.ICoreSearchHotwordService;
+import com.opc.mobile.dto.MaterialSearchDTO;
+import com.opc.mobile.dto.SearchHotwordQueryDTO;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 /**
@@ -27,6 +31,9 @@ public class MobileSearchController extends BaseController
 {
     @Autowired
     private ICoreSearchHotwordService searchHotwordService;
+
+    @Autowired
+    private ICoreMaterialService materialService;
 
     /**
      * 分页查询搜索热词列表
@@ -44,6 +51,26 @@ public class MobileSearchController extends BaseController
 
         PageHelper.startPage(queryDTO.getPageNum(), queryDTO.getPageSize());
         List<CoreSearchHotword> list = searchHotwordService.selectSearchHotwordList(searchHotword);
+        return getDataTable(list);
+    }
+
+    /**
+     * 根据标题模糊搜索素材
+     *
+     * @param searchDTO 搜索参数
+     * @return 分页数据
+     */
+    @Operation(summary = "全局搜索", description = "根据关键字模糊搜索素材标题，分页返回结果")
+    @PostMapping("")
+    public TableDataInfo searchMaterial(@RequestBody MaterialSearchDTO searchDTO)
+    {
+        CoreMaterial material = new CoreMaterial();
+        material.setTitle(searchDTO.getKeyword());
+        // 只查询上线的素材
+        material.setStatus("0");
+
+        PageHelper.startPage(searchDTO.getPageNum(), searchDTO.getPageSize());
+        List<CoreMaterial> list = materialService.selectMaterialList(material);
         return getDataTable(list);
     }
 }
---
+++ opc-admin/src/main/java/com/opc/mobile/dto/MaterialSearchDTO.java
@@ -0,0 +1,48 @@
+package com.opc.mobile.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+/**
+ * 素材搜索DTO
+ *
+ * @author opc
+ */
+@Schema(description = "素材搜索参数")
+public class MaterialSearchDTO {
+
+    /** 搜索关键字（模糊查询） */
+    @Schema(description = "搜索关键字，支持模糊查询", example = "新闻")
+    private String keyword;
+
+    /** 页码 */
+    @Schema(description = "页码，默认为1", example = "1")
+    private Integer pageNum = 1;
+
+    /** 每页大小 */
+    @Schema(description = "每页大小，默认为10", example = "10")
+    private Integer pageSize = 10;
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public Integer getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(Integer pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+}
